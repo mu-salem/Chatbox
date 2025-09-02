36899f406d02480e2db93d642e2c47d4aeea7a2e
OmarKadryDhb <omarkadrydhb.github.io>
2025-09-02 14:52:06 +0300

    OTP

---
+++ src/constants/subjects.js
@@ -0,0 +1,10 @@
+export const subjects = {
+    signUp: "Acctivate Account",
+    forgetPassword: "Forget Password",
+    jobApplication: "New Job Application",
+};
+
+export const OTP_TYPES = {
+    CONFIRM_EMAIL: "Confrim Email",
+    FORGOT_PASSWORD: "Forget Password",
+};
--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -1,18 +1,32 @@
 import { Token } from "../../DB/models/token.model";
-import { asyncHandler } from "../../utils/error handling/asynchandler";
 import User from './../../DB/models/user.model';
-import { jwt } from 'jsonwebtoken';
+import randomstring from "randomstring";
+import { eventEmitter } from './../../utils/emails/email.event';
+import { hash } from "../../utils/hashing/hash";
+import { OTP_TYPES, subjects } from "../../constants/subjects";
 
 export const signup = async(req,res,next) => {
-    const isUser = await User.findOne({email:req.body.email});
+    const {email} = req.body;
+    const isUser = await User.findOne({email});
     
     if (isUser) return next(new Error("User already exists!"), { cause: 400 });
     
-    const hashPass = bcrypt.hashSync(req.body.password, parseInt(process.env.SALT_ROUND));
+    const OTP = randomstring.generate({length:6,charset:"alphanumeric"});
+    eventEmitter.emit("SIGNUP",email,OTP,subjects.signup);
 
-    const user = await User.create({...req.body,password:hashPass});
+    const hashedOTP = hash({plainText:OTP});
 
-    // const token = jwt.sign({email:user.email},process.env.SECRET_KEY)
+    const user = await User.create({
+        ...req.body,
+        isActivated : true,
+        OTP : [
+            {
+                code : hashedOTP,
+                type : OTP_TYPES.SIGNUP,
+                expiresIn : new Date(Date.now() + 10 * 60 * 1000)
+            }
+        ]
+    });
 
     return res.json({success:true,message:"User created successfully!"});
 }
