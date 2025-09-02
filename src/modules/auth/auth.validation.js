41964448c3563a646b1bb285274866409867370e
Mustafa Salem <mustafaasalem06@gmail.com>
2025-09-03 02:35:57 +0300

    fix

--- src/modules/auth/auth.validation.js
+++ src/modules/auth/auth.validation.js
@@ -6,8 +6,7 @@ export const signup = joi.object({
     username: joi.string().required(),
     email: joi.string().email().required(),
     password:joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-    phoneNumber:joi.string().pattern(new RegExp('^[0-9]{10}$')).required(),
-    address:joi.string().required(),
+    phoneNumber:joi.string().required(),
 });
 
 export const confirmOTP = joi.object({
