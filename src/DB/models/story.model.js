27fdf0a4ff6cf454b9e9912ff377349490f6d731
Mustafa Salem <mustafaasalem06@gmail.com>
2025-09-16 23:48:06 +0300

    storySchema

---
+++ src/DB/models/story.model.js
@@ -0,0 +1,25 @@
+import { Types } from "mongoose";
+
+const storySchema = new Schema(
+  {
+    user: { type: Types.ObjectId, ref: "User", required: true },
+    media: {
+      secure_url: { type: String, required: true },
+      public_id: { type: String, required: true },
+      type: { type: String, enum: ["image", "video"], required: true },
+    },
+
+    viewedBy: [
+      {
+        user: { type: Types.ObjectId, ref: "User", required: true },
+        viewedAt: { type: Date, default: Date.now },
+      },
+    ],
+
+    expiresAt: { type: Date, required: true },
+  },
+  { timestamps: true }
+);
+
+const Story = model("Story", storySchema);
+export default Story;
